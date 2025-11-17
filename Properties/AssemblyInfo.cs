17121cb7ba4f082e5617b43ef7a4f41cad208dac
wzhelew <wzhelew@gmail.com>
2025-11-17 13:26:39 +0200

    Remove duplicate assembly metadata

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,12 +1,4 @@
-using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("CloneDBManager")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CloneDBManager")]
 [assembly: ComVisible(false)]
 [assembly: Guid("f7ef7a1a-2747-4db0-8a42-c7cf07cd3b01")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
